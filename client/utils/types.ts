2b77761c59dc0a299d18a48fe03993078213bbef
abhi9ab <abhinabdas004@gmail.com>
2024-12-25 13:43:01 +0530

    restructure the code base 2

--- client/utils/types.ts
+++ client/utils/types.ts
@@ -8,3 +8,38 @@ export interface Interview {
     createdAt: string | null;
     mockId: string;
   }
+
+export interface Answer {
+  id: number,    
+  mockIdRef: string,
+  question: string,
+  correctAnswer: string,
+  userAnswer: string,
+  feedback: string,
+  rating: string,
+  userEmail: string,
+  createdAt: string
+}
+
+export interface AnalysisResult {
+  emotion: {
+    dominant_emotion: string;
+    emotions: {
+      [key: string]: number;
+    };
+  };
+  posture: {
+    status: string;
+    position?: {
+      x_offset: number;
+      y_offset: number;
+    };
+    feedback: string[];
+  };
+  behavior: {
+    movement: string;
+    confidence: number;
+    feedback?: string;
+  };
+  feedback: string[];
+}
